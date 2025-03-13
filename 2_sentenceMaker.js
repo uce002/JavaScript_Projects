3dceb2f63a55de53a8ea23331e33f375a66e2e15
Uzair Yousaf <uzairyousaf1@icloud.com>
2025-03-13 20:58:14 +0000

    made a sort of mad lib program but with a lot of concatenation. not very clean coding

---
+++ 2_sentenceMaker.js
@@ -0,0 +1,21 @@
+let adjective = "colourful";
+let noun = "car";
+let verb = "sleep";
+let place = "space";
+let adjective2 = "cool";
+let noun2 = "rabbit";
+
+let firstStory = "Once upon a time, there was a " + adjective + " " + noun + " who loved to eat " + noun2 + " . The " + noun + " lived in a " + place + " and had " +
+                    adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "smooth";
+noun = "home";
+verb = "walk";
+place = "Amsterdam";
+adjective2 = "old";
+noun2 = "uce";
+
+let secondStory = "As you know, yesterday was a " + adjective + " " + noun + " but I really wanted to go " + noun2 + " . The " + noun + " looked like a " + place + " and had " +
+                    adjective2 + " an automatic gearbox " + verb + ".";
+console.log("Second story: " + secondStory);
