acaf5a23a11b70745acd53d7958191e448083cf4
Uzair Yousaf <uzairyousaf1@icloud.com>
2025-03-13 20:32:50 +0000

    the first project in the freeCodeCamp Full Stack Developer certification. Very simple just declaring variables and concatenating them and later reassigning them.

---
+++ triviaBot.js
@@ -0,0 +1,17 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "uce";
+const botLocation = "Earth";
+let favoriteLanguage = "C#";
+
+console.log("My name is " + botName + "and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Did you know that with " + favoriteLanguage + " you can create videogames, mobile games, websites and more :)";
+console.log(codingFact);
+codingFact = favoriteLanguage + "is mainly used alongside Unity to code games and for you to design and view your work.";
+console.log(codingFact);
+codingFact = "The syntax of " + favoriteLanguage + "is similar to the C-style family such as Java, C, and C++.";
+console.log(codingFact);
+
+console.log("It was fun sharing thes facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
